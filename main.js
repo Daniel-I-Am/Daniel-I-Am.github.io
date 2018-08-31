2f76c3e13b7084afd1721d4ab98c31536f480e01
Daniel de Cloet <djdecloet@gmail.com>
2018-08-31 15:01:03 +0200

    dashboard float fix

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+//function trigered on the dashboard page by input elements
 function checkboxLabelToggle(obj) {
+    //if it is checked now
     if (obj.checked) {
+        //change the class to it's `on` attribute
         obj.previousElementSibling.attributes.class.value = obj.previousElementSibling.attributes.on.value;
+        //move around container below the clicked one
+        if (obj.parentElement.nextElementSibling != null) {
+            obj.parentElement.nextElementSibling.style.transform = "translateY(" + obj.nextElementSibling.clientHeight + "px)";
+        }
+    //if it is not checked now
     } else {
+        //change the class to it's `off` attribute
         obj.previousElementSibling.attributes.class.value = obj.previousElementSibling.attributes.off.value;
+        //move around container below the clicked one
+        if (obj.parentElement.nextElementSibling != null) {
+            obj.parentElement.nextElementSibling.style.transform = "";
+        }
     }
 }
