dafbd6e405d3f6f1d3d7715bf89b47d7ac9b06a7
Daniel de Cloet <djdecloet@gmail.com>
2018-09-04 23:07:20 +0200

    colors to vars

--- scroll.js
+++ scroll.js
@@ -3,7 +3,7 @@ function onScroll() {
     //if window is not at top of screen
     if (window.scrollY>0) {
         //set background of title in header to the same color as the rest of the header
-        document.getElementById('header').style="background-color: var(--navbar-color);"
+        document.getElementById('header').style="background-color: var(--header-background-color);"
     } else {
         //reset
         document.getElementById('header').style=""
