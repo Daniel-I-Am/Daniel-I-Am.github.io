26248e49034cdf6269a762944ca4eff63ef849a5
Daniel de Cloet <djdecloet@gmail.com>
2018-09-06 13:53:56 +0200

    more spacing

--- js/toetsen.js
+++ js/toetsen.js
@@ -15,16 +15,16 @@ fetch('../json/cijfers.json')
                 response.status);
                 return;
             }
-            
+
             // Examine the text in the response
             response.json().then(function(data) {
                 console.log(data)
             });
         }
     )
-.catch(function(err) {
-    console.log('Fetch Error :-S', err);
-});
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
 
 console.log(blokken)
 
